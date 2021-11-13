f466078616a7d0228ccbd804e3a5e44153203899
George Camilleri <georgecamilleri@gmail.com>
2021-11-13 14:32:09 +0100

    20211113T143100: Next: Continue GeneticAlgorithm (generation of population using randomisation)

--- src/GeneticAlgorithm.java
+++ src/GeneticAlgorithm.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import static java.lang.System.gc;
 
 /*
@@ -10,7 +11,7 @@ Provides methods to solves the TSP using a Genetic Algorithm by:
 public class GeneticAlgorithm {
 
 	private long numOfPermutations = -1;
-	private int[] localities;
+	private ArrayList<Integer> localities = new ArrayList();
 	private String[] population;
 
 	public GeneticAlgorithm() {}
@@ -39,24 +40,26 @@ public class GeneticAlgorithm {
 
 	/*
 	Create the population using randomisation. This method will create random
-	patterns of the locality numbers in between the standing and end localities,
+	patterns of the locality numbers in between the start and end localities,
 	which is always locality 1.
+	One of the population (journeys) will be the Best Neighbour, since this is
+	proven to return a good (if not the best) journey.
 	*/
 	private void createPopulation (int populationNumber) {
 		try {
 			// Re-initialise the existing population
 			this.population = new String[populationNumber];
 
-			int p = 0;	// counter
-			String randomPop = "";	// Randomised population
-			for (int i : localities) {
-				randomPop += localities[i];
-				if (i < (localities.length - 1)) {
-					randomPop += "|";	// Separate locality number with a pipe
-				}
-			}
-			while (p <= populationNumber) {
 
+			int p = 0;	// counter
+			String randomPop = "";	// Randomised population (pipe-delimited)
+
+			// First element of population array will be the BeFS output
+			this.population[p] = convertListToPipeDelimited(null
+					, Utilities.solveTSP_GreedyBeFS(localities), false);
+
+			while (p <= populationNumber) {
+				// Randomise (slightly) the GreedyBeFS list until population number is reached
 				p++;
 			}
 
@@ -68,6 +71,58 @@ public class GeneticAlgorithm {
 		}
 	}
 
+	/*
+	Returns a random number within the specified range
+	*/
+	private int getRandom(int min, int max) {
+		try {
+			return (int) ((Math.random() * ((max + 1) - min)) + min);
+		} catch (Exception e) {
+			System.out.println("GeneticAlgorithm.getRandom - An error has occurred - " + e.getMessage());
+			e.printStackTrace();
+			return -1;
+		} finally {
+			gc();
+		}
+	}
+
+	/*
+	Transforms an Integer array into a pipe-delimited String
+	*/
+	private String convertListToPipeDelimited (int[] inputA, ArrayList<Integer> inputAL, boolean useArray) {
+		String output = "";
+		try {
+			if ((inputA != null) && useArray) {
+				for (int i : inputA) {
+					output += i;
+					if (i != (inputA[inputA.length - 1])) {
+						output += "|";
+					}
+				}
+			} else {
+				if (inputAL != null) {
+					for (int i : inputAL) {
+						output += i;
+						if (i != (inputAL.get(inputAL.size() - 1))) {
+							output += "|";
+						}
+					}
+				}
+			}
+			return output;
+		} catch (Exception e) {
+			System.out.println("GeneticAlgorithm.convertListToPipeDelimited - An error has occurred - " + e.getMessage());
+			e.printStackTrace();
+			return "";
+		} finally {
+			gc();
+		}
+	}
+
+	/*
+	Solves the TSP using a Genetic Algorithm, by starting with a population of
+	journeys and evolving them into better (shorter) ones.
+	 */
 	public void solveTSP_GA () {
 
 	}
